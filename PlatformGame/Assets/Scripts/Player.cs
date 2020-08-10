90960b99ebf57518a789eb93d3d3e23834238213
Cadu (Carlos Eduardo) <ceto.snf17@uea.edu.br>
2020-08-10 09:09:21 -0400

    Update; Adicionando Movimentação
    Adicionando a movimentação lateral e pulo

---
+++ PlatformGame/Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private Rigidbody2D rigid;
+
+    public bool isJuming = false;
+    public bool doubleJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * speed;
+
+    }
+
+    void Jump()
+    {
+        if (Input.GetButton("Jump") && !isJuming)
+        {
+            rigid.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJuming = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJuming = true;
+        }
+    }
+
+}
