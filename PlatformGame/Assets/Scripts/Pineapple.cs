1a86a6f01f614fc18de52690770239314432bf10
Cadu (Carlos Eduardo) <ceto.snf17@uea.edu.br>
2020-08-10 12:19:54 -0400

    Update: Criando itens
    Foram criados o prefab do item Pineapple, a animação de coleta e a destruição do objeto.
    Prefab gerado:
    - Pineapple
    - Collected

---
+++ PlatformGame/Assets/Scripts/Pineapple.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pineapple : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private CapsuleCollider2D capCollider;
+
+    public GameObject collected;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        capCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            spriteRenderer.enabled = false;
+            capCollider.enabled = false;
+            collected.SetActive(true);
+
+            Destroy(gameObject, 0.4f);
+        }
+    }
+}
